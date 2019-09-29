58ca65d03fa54cd3a05f337bfbadbd7fe6fa7ea4
helena2505 <helena2505>
2019-09-29 15:39:57 +0300

    Drag and drop with interactjs v1

--- main.js
+++ main.js
@@ -1,13 +1,65 @@
-var locMenu = document.getElementById('dynMenu');
-var lst = document.getElementById('options');
-variable.onclick = function() {
-	var li = document.createElement('LI'), li1 = document.createElement('LI'), li2 = document.createElement('LI'), li3 = document.createElement('LI');
-	li.innerHTML = 'Нарисовать';
-	lst.appendChild(li);
-	li1.innerHTML = 'Присвоить значение';
-	lst.appendChild(li1);
-	li2.innerHTML = 'Изменить значение';
-	lst.appendChild(li2);
-	li3.innerHTML = 'Поменять с другой переменной';
-	lst.appendChild(li3);
+interact('.dropzone').dropzone({
+    // only accept elements matching this CSS selector
+    accept: '#yes-drop',
+    // Require a 75% element overlap for a drop to be possible
+    overlap: 0.75,
+
+    // listen for drop related events:
+
+    ondropactivate: function (event) {
+        // add active dropzone feedback
+        event.target.classList.add('drop-active')
+    },
+    ondragenter: function (event) {
+        var draggableElement = event.relatedTarget
+        var dropzoneElement = event.target
+
+        // feedback the possibility of a drop
+        dropzoneElement.classList.add('drop-target')
+        draggableElement.classList.add('can-drop')
+        draggableElement.textContent = 'Dragged in'
+    },
+    ondragleave: function (event) {
+        // remove the drop feedback style
+        event.target.classList.remove('drop-target')
+        event.relatedTarget.classList.remove('can-drop')
+        event.relatedTarget.textContent = 'Dragged out'
+    },
+    ondrop: function (event) {
+        event.relatedTarget.textContent = 'Dropped'
+    },
+    ondropdeactivate: function (event) {
+        // remove active dropzone feedback
+        event.target.classList.remove('drop-active')
+        event.target.classList.remove('drop-target')
+    }
+})
+
+interact('.drag-drop')
+    .draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        autoScroll: true,
+        // dragMoveListener from the dragging demo above
+        onmove: dragMoveListener
+    })
+function dragMoveListener (event) {
+    var target = event.target
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)'
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x)
+    target.setAttribute('data-y', y)
 }
