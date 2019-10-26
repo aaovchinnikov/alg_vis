4c5d463516ea4b0ff55533fb68c81ee4e6219c1c
helena2505 <helena2505>
2019-10-26 11:54:33 +0300

    Added function for removing a container

---
+++ delete_container.js
@@ -0,0 +1,15 @@
+let containerMenu = document.getElementById('available-containers');
+let allPoints = containerMenu.querySelectorAll('li');
+
+for (let i = 0; i < allPoints.length - 1; i++) {
+    let curContainer = allPoints[i];
+    curContainer.addEventListener("contextmenu", deleteContainer);
+}
+
+function deleteContainer(event) {
+    event.preventDefault();
+    let deletedContainer = document.getElementById(event.target.id);
+    console.log(deletedContainer);
+    //containerMenu.removeChild(deletedContainer);
+    alert('here!');
+}
