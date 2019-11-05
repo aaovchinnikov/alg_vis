7e43827472e7c3c136e1f96b52dc383be806eb5e
helena2505 <helena2505>
2019-11-05 20:19:44 +0300

    Server scripts fixed and improved

--- include/test.php
+++ include/test.php
@@ -4,4 +4,10 @@
     $got_descr = $_POST["descr"];
     $req = "INSERT INTO containers (container_name, description) VALUES ( '".$got_name."', '".$got_descr."');";
     $result = mysqli_query($link, $req);
-    echo $result;
+    $req = "SELECT id FROM containers WHERE container_name = '".$got_name."';";
+    $result1 = mysqli_query($link, $req);
+    $id = mysqli_fetch_all($result1, MYSQLI_ASSOC);
+    foreach ($id as $id1):
+        $k = $id1["id"];
+    endforeach;
+    echo $result." ".$k;
