5d383bc9e6f5802abfe377384ec3559c833e44be
helena2505 <elenakarelina833@gmail.com>
2020-03-14 19:27:44 +0300

    Namespace for svg set

--- include/interactive.php
+++ include/interactive.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'database.php';
-$scene_id = "158";
+$scene_id = "160";
 $STH = $DB->prepare("SELECT xml_code FROM scenes WHERE s_id = :id");
 $STH->setFetchMode(PDO::FETCH_NUM);
 if(!$STH->execute(array("id" => $scene_id))) {
@@ -11,7 +11,17 @@ $xml_code = $STH->fetchAll()[0][0];
 $xml_code = mb_substr($xml_code, 26);
 $xml_code = base64_decode($xml_code);
 echo $xml_code;
-preg_match_all("/[<][a-z].*?input_a.*?>/", $xml_code, $match, PREG_OFFSET_CAPTURE);
+$xml_code = str_replace('xmlns=', 'ns=', $xml_code);
+$parse = new SimpleXMLElement($xml_code);
+echo PHP_EOL;
+print_r($parse->xpath("//switch[text = 'input_a']/text"));
+//print_r($parse->g->g->switch->text);
+/*$parse->registerXPathNamespace('svg', 'http://www.w3.org/2000/svg');
+$parse->registerXPathNamespace('xlink', 'http://www.w3.org/1999/xlink');
+$result = $parse->xpath('//svg:g');
+$content = $result[0];
+//print_r($content->xpath('rect'));
+/*preg_match_all("/[<][a-z].*?input_a.*?>/", $xml_code, $match, PREG_OFFSET_CAPTURE);
 $match = $match[0];
 foreach($match as $elem):
     $code = $elem[0];
@@ -22,7 +32,7 @@ endforeach;
 echo PHP_EOL;
 $xml_code = str_replace("input_a", "222", $xml_code);
 echo $xml_code;
-/*print_r($match);
+print_r($match);
 print_r('\n');
 $xml_code = str_replace("input_a", "222", $xml_code);
 $xml_code = preg_replace('/[#][A-F | 0-9]{6}/', "#FF0000", $xml_code);
